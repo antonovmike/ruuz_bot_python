6b62055bd839785e7cba717cdf8e6f6133c72d51
antonovmike <antonvma@outlook.com>
2023-02-11 20:08:09 +0500

    grammar cases

--- main.py
+++ main.py
@@ -17,29 +17,6 @@ def start(message):
     help = types.InlineKeyboardButton(text='Об этом боте', callback_data='help')
     mainmenu.add(conversation, grammar, help)
     bot.send_message(chat_id=message.chat.id, text='Главное меню', reply_markup=mainmenu)
-    # kb = [[types.KeyboardButton('/command1')],
-    #       [types.KeyboardButton('/command2')]]
-    # kb_markup = types.ReplyKeyboardMarkup(True)
-    # bot.send_message(chat_id=message.chat_id, text="your message", reply_markup=kb_markup)
-    # if message.text:
-    #     mainmenu = types.InlineKeyboardMarkup()
-    #     key1 = types.InlineKeyboardButton(text='Общение', callback_data='key1')
-    #     key2 = types.InlineKeyboardButton(text='Грамматика', callback_data='key2')
-    #     mainmenu.add(key1, key2)
-    #     bot.send_message(chat_id=message.chat.id, text='Главное меню', reply_markup=mainmenu)
-        # keyboard = types.InlineKeyboardMarkup()
-        # key_greetings = types.InlineKeyboardButton(text='Приветствие / Greeting', callback_data='greetings')
-        # keyboard.add(key_greetings)
-        # key_how_are_you = types.InlineKeyboardButton(text='Как ваши дела? / How are you?', callback_data='how_are_you')
-        # keyboard.add(key_how_are_you)
-        # key_by= types.InlineKeyboardButton(text='До свидания / Goodby', callback_data='by')
-        # keyboard.add(key_by)
-        # key_time_verbs= types.InlineKeyboardButton(text='Время глаголов / Verbs tenses', callback_data='time_verbs')
-        # keyboard.add(key_time_verbs)
-        # key_help= types.InlineKeyboardButton(text='Про этот бот / About this bot', callback_data='help')
-        # keyboard.add(key_help)
-        # question = 'Что вы хотите перевести?'
-        # bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -62,8 +39,10 @@ def callback_inline(call):
     elif call.data == "grammar":
         next_menu2 = types.InlineKeyboardMarkup()
         time_verbs = types.InlineKeyboardButton(text='Время глаголов / Verbs tenses', callback_data='time_verbs')
+        cases = types.InlineKeyboardButton(text='Падежи / Cases', callback_data='cases')
+        case_genitive = types.InlineKeyboardButton(text='Родительный падеж / Case genitive', callback_data='case_genitive')
         back = types.InlineKeyboardButton(text='В главное меню', callback_data='mainmenu')
-        next_menu2.add(time_verbs, back)
+        next_menu2.add(time_verbs, cases, case_genitive, back)
         bot.edit_message_text('Тема: Грамматика', call.message.chat.id, call.message.message_id, reply_markup=next_menu2)
     if call.data == "greetings":
         txt = Path('dictionaries/greetings').read_text()
@@ -79,6 +58,10 @@ def callback_inline(call):
         bot.send_message(call.message.chat.id, txt)
     if call.data == "time_verbs":
         bot.send_photo(call.message.chat.id, 'https://github.com/antonovmike/ruuz_bot_python/blob/test/pictures/uz_verb_forms.png?raw=true')
+    if call.data == "cases":
+        bot.send_photo(call.message.chat.id, 'https://github.com/antonovmike/ruuz_bot_python/blob/test/pictures/cases.png?raw=true')
+    if call.data == "case_genitive":
+        bot.send_photo(call.message.chat.id, 'https://github.com/antonovmike/ruuz_bot_python/blob/test/pictures/case_genitive.png?raw=true')
 
 
 bot.polling(none_stop=True, interval=1)
