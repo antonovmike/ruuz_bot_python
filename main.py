aec5eced1c7f1dd131a3d2f71ffcbce495f11a3f
antonovmike <antonvma@outlook.com>
2023-02-11 18:44:53 +0500

    fmt

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ bot = telebot.TeleBot(env[:index])
 
 @bot.message_handler(content_types=['text'])
 def start(message):
+    # kb = [[types.KeyboardButton('/command1')],
+    #       [types.KeyboardButton('/command2')]]
+    # kb_markup = types.ReplyKeyboardMarkup(True)
+    # bot.send_message(chat_id=message.chat_id, text="your message", reply_markup=kb_markup)
     if message.text:
         keyboard = types.InlineKeyboardMarkup()
         key_greetings = types.InlineKeyboardButton(text='Приветствие / Greeting', callback_data='greetings')
@@ -19,8 +23,8 @@ def start(message):
         keyboard.add(key_how_are_you)
         key_by= types.InlineKeyboardButton(text='До свидания / Goodby', callback_data='by')
         keyboard.add(key_by)
-        # key_time_verbs= types.InlineKeyboardButton(text='Время глаголов / Verbs tenses', callback_data='time_verbs')
-        # keyboard.add(key_time_verbs)
+        key_time_verbs= types.InlineKeyboardButton(text='Время глаголов / Verbs tenses', callback_data='time_verbs')
+        keyboard.add(key_time_verbs)
         key_help= types.InlineKeyboardButton(text='Про этот бот / About this bot', callback_data='help')
         keyboard.add(key_help)
         question = 'Что вы хотите перевести?'
@@ -39,11 +43,10 @@ def callback_worker(call):
         txt = Path('dictionaries/by').read_text()
         bot.send_message(call.message.chat.id, txt)
     if call.data == "time_verbs":
-        bot.send_photo(call.message.chat.id, 'pictures/uz_verb_forms.png')
+        bot.send_photo(call.message.chat.id, 'https://github.com/antonovmike/ruuz_bot_python/blob/test/pictures/uz_verb_forms.png?raw=true')
     if call.data == "help":
         txt = Path('dictionaries/help').read_text()
         bot.send_message(call.message.chat.id, txt)
-        bot.send_photo(call.message.chat.id, 'https://github.com/antonovmike/ruuz_bot_python/blob/main/Screenshot.png')
 
 
 bot.polling(none_stop=True, interval=1)
