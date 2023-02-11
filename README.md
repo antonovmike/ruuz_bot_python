ba3ff9f5659d4f5782ad720ebad34d8420733150
antonovmike <antonvma@outlook.com>
2023-02-11 08:29:24 +0500

    new dictionaries

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # ruuz_bot_python
 
-![ruuz_bot_python](https://github.com/antonovmike/ruuz_bot_python/blob/main/Screenshot.png)
+![ruuz_bot_python](https://github.com/antonovmike/ruuz_bot_python/blob/test/Screenshot.png)
 
 (venv)
 ```commandline
 python -m pip install --upgrade pip
 pip install pytelegrambotapi
+pip install aiogram
 ```
 
 @bot.message_handler(content_types=['text', 'document', 'audio'])
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot import types
+from pathlib import Path
 
 file = open('./test.env')
 env = file.readlines()[0]
@@ -12,28 +13,39 @@ bot = telebot.TeleBot(env[:index])
 def start(message):
     if message.text:
         keyboard = types.InlineKeyboardMarkup()
-        key_greetings = types.InlineKeyboardButton(text='Приветствие', callback_data='greetings')
+        key_greetings = types.InlineKeyboardButton(text='Приветствие / Greeting', callback_data='greetings')
         keyboard.add(key_greetings)
-        key_how_are_you = types.InlineKeyboardButton(text='Как ваши дела?', callback_data='how_are_you')
+        key_how_are_you = types.InlineKeyboardButton(text='Как ваши дела? / How are you?', callback_data='how_are_you')
         keyboard.add(key_how_are_you)
-        key_fine_and_you= types.InlineKeyboardButton(text='Я в порядке. Спасибо. А вы?', callback_data='fine_and_you')
-        keyboard.add(key_fine_and_you)
-        key_by= types.InlineKeyboardButton(text='До свидания', callback_data='by')
+        key_by= types.InlineKeyboardButton(text='До свидания / Goodby', callback_data='by')
         keyboard.add(key_by)
+        key_help= types.InlineKeyboardButton(text='Про этот бот / About this bot', callback_data='help')
+        keyboard.add(key_help)
         question = 'Что вы хотите перевести?'
         bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+
+
+f = open("example_1.txt","r")
+lines = f.readlines()
+index = len(lines[1])-1
+item = lines[2]
+number = item[:2]
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     if call.data == "greetings":
-        bot.send_message(call.message.chat.id, 'ASSALOM ALEYKUM - вежливое приветствие\n\nVAALEYKUM ASSALOM - вежливый ответ на приветствие\n\nSALOM - неформальное приветствие, используется только в отношении хороших знакомых')
+        txt = Path('dictionaries/greetings').read_text()
+        bot.send_message(call.message.chat.id, txt)
     if call.data == "how_are_you":
-        bot.send_message(call.message.chat.id, 'YAXSHIMI SIZ? - как Ваши дела?\n\nTAZUVMI SIZ? - как Ваши дела?\n\nISHLARINGIZ QALAY? - как твои дела?')
-    if call.data == "fine_and_you":
-        bot.send_message(call.message.chat.id, 'HA YAXSHI, RAHMAT. SIZDA CHI? - Хорошо, спасибо. А у Вас?\n\nMEN YAXSHIMAN. RAHMAT! SIZCHI? - Хорошо, спасибо, а Ваши?')
+        txt = Path('dictionaries/how_are_you').read_text()
+        bot.send_message(call.message.chat.id, txt)
     if call.data == "by":
-        bot.send_message(call.message.chat.id, 'KO’RISHKUNCHA / KO’RISHKUNCHA XAYR - до свидания\n\nXAYR - пока\n\nUCHRASHUVGACHA/UCHRASHAMIZ - увидимся\n\nSAGIBOLING - пока / будьте здоровы')
+        txt = Path('dictionaries/by').read_text()
+        bot.send_message(call.message.chat.id, txt)
+    if call.data == "help":
+        txt = Path('dictionaries/help').read_text()
+        bot.send_message(call.message.chat.id, txt)
 
 
 bot.polling(none_stop=True, interval=1)
